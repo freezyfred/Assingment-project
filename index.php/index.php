753976f1119fab0c0a7df0b61793e4870a7eac56
fred <145464850+freezyfred@users.noreply.github.com>
2023-10-21 15:06:08 +0300

    Assingment submission for 'fredmotrs' project

---
+++ index.php/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car Sales</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Car Sales</h1>
+    </header>
+
+    <section id="about">
+        <h2>About Us</h2>
+        <p>We are your trusted source for high-quality cars. With years of experience in the automotive industry, we provide the best deals and excellent customer service.</p>
+    </section>
+
+    <section id="services">
+        <h2>Our Services</h2>
+        <ul>
+            <li>Wide selection of new and used cars</li>
+            <li>Competitive financing options</li>
+            <li>Professional car inspection and maintenance</li>
+            <li>Expert car-buying advice</li>
+        </ul>
+    </section>
+
+    <section id="contact">
+        <h2>Contact Us</h2>
+        <p>Phone: 0703-381-921</p>
+        <p>Email: fredericktoye176@gmail.com</p>
+    </section>
+</body>
+</html>
